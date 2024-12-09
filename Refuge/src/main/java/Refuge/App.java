7ff11c8d88b2ddd5a286b9e6c301ea2fe2a74a77
MicoWata <michael.gagnon1987@gmail.com>
2024-12-09 09:53:52 -0500

    Ouvrir la fenetre au demarrage

--- Refuge/src/main/java/Refuge/App.java
+++ Refuge/src/main/java/Refuge/App.java
@@ -1,7 +1,12 @@
 package Refuge;
 
+import Refuge.Swinger.Window;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // System.out.println("Hello World!");
+
+    Window window = new Window();
+    window.setVisible(true);
   }
 }
--- Refuge/src/main/java/Refuge/Swinger/Window.java
+++ Refuge/src/main/java/Refuge/Swinger/Window.java
@@ -1,8 +1,36 @@
 package Refuge.Swinger;
 
-public class Window {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+
+public class Window extends JFrame {
+  private static Point size = new Point(1200, 800);
 
   public Window() {
     // Initialize object
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      System.out.println("Error : No look, no feel");
+    }
+
+    SwingUtilities.invokeLater(() -> {
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+      // Application app = new Application();
+
+      setBounds(
+          (screenSize.width - size.x) / 2,
+          (screenSize.height - size.y) / 2,
+          size.x, size.y);
+
+      setExtendedState(JFrame.NORMAL);
+      //
+      // app.setVisible(true);
+    });
   }
 }
